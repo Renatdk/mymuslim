9c19b68859673a4bc8133ecc60b17801f5b731f1
Renat <renatd.k@gmail.com>
2017-02-20 22:35:15 +0600

    add change

--- main.go
+++ main.go
@@ -20,8 +20,12 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
+/*	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+*/
+	router.GET("/list", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "list.tmpl.html", nil)
 	})
 
 	router.Run(":" + port)
